714d0bef86bff1971bcea561b7f4a50ecd748d8e
wangjiaming0909 <604227650@qq.com>
2018-07-09 13:32:42 +0800

    2

--- well_info/Program.cs
+++ well_info/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace well_info {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("hello world!");
         }
     }
 }
